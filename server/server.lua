555a315ecf5377db67fbb75e334e5a1223a2f604
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-15 03:01:46 -0700

    !refactor: ox callbacks, dynamic pricing

--- server/server.lua
+++ server/server.lua
@@ -19,57 +19,19 @@ MySQL.ready(function()
 	)
 end)
 
+-- Events
+
 RegisterServerEvent('fivem-appearance:save')
 AddEventHandler('fivem-appearance:save', function(appearance)
-	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.Async.execute('UPDATE users SET skin = @skin WHERE identifier = @identifier', {
-		['@skin'] = json.encode(appearance),
-		['@identifier'] = xPlayer.identifier
-	})
-end)
-
-ESX.RegisterServerCallback('fivem-appearance:getPlayerSkin', function(source, cb)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.Async.fetchAll('SELECT skin FROM users WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
-	}, function(users)
-		local user, appearance = users[1]
-		if user.skin then
-			appearance = json.decode(user.skin)
-		end
-		cb(appearance)
-	end)
+	MySQL.update('UPDATE users SET skin = ? WHERE identifier = ?', {json.encode(appearance), xPlayer.identifier})
 end)
 
 RegisterServerEvent("fivem-appearance:saveOutfit")
 AddEventHandler("fivem-appearance:saveOutfit", function(name, pedModel, pedComponents, pedProps)
 	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.Async.insert('INSERT INTO `outfits` (`identifier`, `name`, `ped`, `components`, `props`) VALUES (@identifier, @name, @ped, @components, @props)', {
-		['@ped'] = json.encode(pedModel),
-		['@components'] = json.encode(pedComponents),
-		['@props'] = json.encode(pedProps),
-		['@name'] = name,
-		['@identifier'] = xPlayer.identifier
-	})
-end)
-
-ESX.RegisterServerCallback('fivem-appearance:getOutfits', function(source, cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local outfits = {}
-    MySQL.Async.fetchAll('SELECT id, name, ped, components, props FROM outfits WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
-	}, function(result)
-		for i=1, #result, 1 do
-			table.insert(outfits, {id = result[i].id, name = result[i].name, ped = json.decode(result[i].ped), components = json.decode(result[i].components), props = json.decode(result[i].props)})
-		end
-        if outfits then
-		    cb(outfits)
-        else
-            cb(false)
-        end
-	end)
+	MySQL.insert.await('INSERT INTO outfits (identifier, name, ped, components, props) VALUES (?, ?, ?, ?, ?)', {xPlayer.identifier, name, json.encode(pedModel), json.encode(pedComponents), json.encode(pedProps)})
 end)
 
 RegisterServerEvent("fivem-appearance:deleteOutfit")
@@ -81,7 +43,59 @@ AddEventHandler("fivem-appearance:deleteOutfit", function(id)
 	})
 end)
 
--- ESX Skin Compatibility
+-- Callbacks
+
+lib.callback.register('fivem-appearance:getPlayerSkin', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local users = MySQL.query.await('SELECT skin FROM outfits users identifier = ?', {xPlayer.identifier})
+	if users then
+		local user, appearance = users[1]
+		if user.skin then
+			appearance = json.decode(user.skin)
+		end
+	end
+	return appearance
+end)
+
+lib.callback.register('fivem-appearance:payFunds', function(source, price)
+    local xPlayer = ESX.GetPlayerFromId(source)
+	local xAccountMoney = xPlayer.getAccount(Config.PaymentAccount).money 
+	if xAccountMoney < price then 
+		return false 
+	else
+		xPlayer.removeAccountMoney(Config.PaymentAccount, price)
+		return true
+	end
+end)
+
+lib.callback.register('fivem-appearance:getOutfits', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+    local outfits = {}
+    local result = MySQL.query.await('SELECT * FROM outfits WHERE identifier = ?', {xPlayer.identifier})
+	if result then
+		for i=1, #result, 1 do
+			outfits[#outfits + 1] = {
+				id = result[i].id,
+				name = result[i].name,
+				ped = json.decode(result[i].ped),
+				components = json.decode(result[i].components),
+				props = json.decode(result[i].props)
+			}
+		end
+		return outfits
+	else
+		return false
+	end
+end)
+
+-- Commands
+ESX.RegisterCommand('skin', 'admin', function(xPlayer, args, showError)
+	args.playerId.triggerEvent('fivem-appearance:skinCommand')
+end, false, {help = Strings.skin_command_help, validate = true, arguments = {
+	{name = 'playerId', help = Strings.skin_command_arg_help, type = 'player'}
+}})
+
+-- esx_skin/skinchanger compatibility
 getGender = function(model)
     if model == 'mp_f_freemode_01' then
         return 1
@@ -92,9 +106,8 @@ end
 
 ESX.RegisterServerCallback('esx_skin:getPlayerSkin', function(source, cb)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.Async.fetchAll('SELECT skin FROM users WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
-	}, function(users)
+	local users = MySQL.query.await('SELECT skin FROM users WHERE identifier = ?', {xPlayer.identifier})
+	if users then
 		local user, appearance = users[1]
 		local jobSkin = {
 			skin_male   = xPlayer.job.skin_male,
@@ -105,30 +118,5 @@ ESX.RegisterServerCallback('esx_skin:getPlayerSkin', function(source, cb)
 		end
 		appearance.sex = getGender(appearance.model)
 		cb(appearance, jobSkin)
-	end)
+	end
 end)
-
-ESX.RegisterServerCallback('fivem-appearance:payfee', function(source, cb, param)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local money = xPlayer.getMoney()
-
-    if param then
-        local cost = Config.Price[param]
-        if money >= cost then
-            if cost > 0 then
-                xPlayer.removeMoney(cost)
-                TriggerClientEvent('fivem-appearance:notify', source, 'Success', 'You have paid $'..cost, 'success')
-            end
-            cb(true)
-        else
-            TriggerClientEvent('fivem-appearance:notify', source, 'Insufficient Funds', 'You need $'..cost..' dollars in your pocket!', 'error')
-            cb(false)
-        end
-    end
-end)
-
-ESX.RegisterCommand('skin', 'admin', function(xPlayer, args, showError)
-	args.playerId.triggerEvent('fivem-appearance:skinCommand')
-end, false, {help = 'Change Skin', validate = true, arguments = {
-	{name = 'playerId', help = 'Player ID', type = 'player'}
-}})
